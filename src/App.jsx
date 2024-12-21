6808d45a2f7cd3ccba305d8d6d8ebd5a0ebd1b11
Pinki Jha <pinkijha1310@gmail.com>
2024-12-22 01:42:40 +0530

    done with the setup of tailwind and react+vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-blue-700'>Ration pani</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
