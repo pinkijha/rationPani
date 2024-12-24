71f015e310df2611af5208e1e0608e4ce07f4360
Pinki Jha <pinkijha1310@gmail.com>
2024-12-24 20:04:08 +0530

    add some css on header mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,19 @@ import { LiaClipboardListSolid } from "react-icons/lia";
 
 const Header = () => {
   return (
-    <div className='flex md:p-3 justify-between bg-slate-100 md:h-16 md:w-full shadow-gray-300 shadow-md'>
+    <div className='flex md:p-3 justify-between bg-slate-100  md:h-16 md:w-full shadow-gray-300 shadow-md'>
       <div className='flex justify-between space-x-2 items-center'>
-        <img className='cursor-pointer rounded-full md:h-12'
+        <img className='cursor-pointer rounded-full h-10 md:h-12'
          src={IMG_LOGO} />
         <h1 className='cursor-pointer font-bold md:text-lg text-gray-700'><span className='text-green-700 italic'>R</span>ation
          <span className='text-green-700 italic'>P</span>ani</h1>
       </div>
 
-      <div>
+      <div className='md:flex hidden'>
         <SearchBar/>
       </div>
 
-      <div className='flex items-center  md:hover:scale-110 duration-300 hover:rounded-full'>
+      <div className='flex items-center md:mx-1 m-3 hover:scale-90  md:hover:scale-110 duration-300 hover:rounded-full'>
       <LiaClipboardListSolid className='text-green-700 
      cursor-pointer md:mr-2' size={32} />
       </div>
